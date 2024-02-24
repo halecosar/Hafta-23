590f087a05d9b0e493b939fa20edeb2ca18c4f4a
halecosar <halealtunakar@hotmail.com>
2024-02-24 20:34:19 +0300

    last

--- to_do_app/src/App.jsx
+++ to_do_app/src/App.jsx
@@ -1,26 +1,104 @@
-import React from 'react'
-import Header from './components/header'
-import Main from './components/main'
-import Footer from './components/footer'
-import FooterInfo from './components/footerInfo'
+import React, { useState } from "react";
+import Todo from "./components/Todo";
+import "./App.css";
 
 
 function App() {
+  const [text, setText] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [filter, setFilter] = useState('all');
+
+  const handleChange = (e1) => {
+    setText(e1.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const addTodo = {
+      id: Date.now(),
+      text: text,
+      completed: false
+    };
+    setTodoList([...todoList, addTodo])
+    setText(""); // Yeni bir todo eklerken öncekinin inputun valuesunda kalmasını engellemek için boşaltıldı.
+  };
+
+  const filteredTodoList = todoList.filter(todo => {
+    if (filter === 'all') {
+      return true;
+    } else if (filter === 'active') {
+      return !todo.completed;
+    } else if (filter === 'completed') {
+      return todo.completed;
+    }
+  });
+
+  function clearCompleted() {
+    let filteredTodo = todoList.filter((todo) => todo.completed !== true);
+    setTodoList(filteredTodo);
+  }
+
+
   return (
-    <div>
+    <>
       <section className="todoapp">
-        <Header></Header>
-        <Main></Main>
-        <Footer></Footer>
+        <header className="header">
+          <h1>todos</h1>
+          <form className="inputForm" onSubmit={handleSubmit}>
+            <input
+              className="new-todo"
+              placeholder="What needs to be done?"
+              autoFocus
+              value={text}
+              onChange={handleChange}
+            />
+          </form>
+        </header>
 
+        <section className="main">
+          <input className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            {filteredTodoList.map(todo => (
+              <Todo
+                key={todo.id}
+                selectedTodo={todo}
+                setTodoList={setTodoList}
+                todoList={todoList}
+              />
+            ))}
+          </ul>
+        </section>
 
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>{todoList.length} </strong>
+            items left
+          </span>
+
+          <ul className="filters">
+            <li>
+              <a href="#/" onClick={() => setFilter('all')}>
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/" onClick={() => setFilter('active')}>
+                Active
+              </a>
+            </li>
+            <li>
+              <a href="#/" onClick={() => setFilter('completed')}>
+                Completed
+              </a>
+            </li>
+          </ul>
+
+          <button className="clear-completed" onClick={clearCompleted}>Clear completed</button>
+        </footer>
       </section>
-
-      <FooterInfo></FooterInfo>
-
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
-
+export default App;

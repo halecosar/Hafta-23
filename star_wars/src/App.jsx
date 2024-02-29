453cdd7d52cdcf765d084f9fbf3b384e94000467
halecosar <halealtunakar@hotmail.com>
2024-02-29 10:59:21 +0300

    last

--- star_wars/src/App.jsx
+++ star_wars/src/App.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from 'react'
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+import Starship from './Components/Starship';
+import Details from './Components/Details';
 
 function App() {
-  const [data, setData] = useState({});
-  const [data2, setData2] = useState(0);
-
-  useEffect(() => {
-    fetch("https://swapi.dev/api/people/1/")
-      .then(r => r.json()) //jsona çevirdik.
-      .then(a => {
-        setData(a);
-      })
-  }, [])
-
-  console.log(data);
-
   return (
-    <div>
-      {data.name}
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Starship />} />
+        <Route path="/starships/:id/" element={<Details />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
